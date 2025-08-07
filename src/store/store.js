003d6c584e63db31969c91be96158fb47cb50e66
kirtanpatel1411 <bhimanikirtan7@gmail.com>
2025-08-07 17:34:46 +0530

    display all fetch teachers in table in manageteacher page

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../redux/userSlice";
+import schoolReducer from "../redux/schoolSlice";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    school: schoolReducer,
   },
 });
